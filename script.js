584645fd4e880b72464011cae9c386c245ed2828
dnlKaizer <danilokaizer@gmail.com>
2024-08-27 19:50:12 -0300

    functions getWord(index) e setWordStatusOn(index)

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} index 
+ * @returns {HTMLInputElement[]}
+ */
+let getWord = (index) => {
+    let word = []
+    for (let i = 0; i < 5; i++) {
+        word[i] = document.querySelector("#row" + index + " .l" + (i + 1));
+    }
+    return word;
+}
+
+/**
+ * @param {number} index 
+ */
+let setWordStatusOn = (index) => {
+    let word = getWord(index);
+    word.forEach((inputElement, i) => {
+        inputElement.classList = "letter letterOn l" + i;
+        inputElement.disabled = false;
+    });
+}
