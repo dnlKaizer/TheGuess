e024fad0acdcf66b2bc9eeb5b4096f08a6043e46
dnlKaizer <danilokaizer@gmail.com>
2024-08-29 21:01:47 -0300

    Comparadores de comandos do teclado + método que adiciona eventos nas letters

--- script.js
+++ script.js
@@ -4,6 +4,58 @@ const cols = 5;
 let currentRow = 0;
 let currentCol = 0;
 
+function addLettersEvents() {
+    for (let i = 0; i < 6; i++) {
+        getWord(i).forEach((element, j) => {
+            element.addEventListener("keyup", (event) => {
+                const str = event.key;
+                if ((isLetter(str) || isArrowRight(str) || isDelete(str)) && j + 1 < 6) {
+                    getLetter(i,j + 1).focus();
+                }
+                if ((isBackspace(str) || isArrowLeft(str)) && j - 1 >= 0) {
+                    getLetter(i, j - 1).focus();
+                }
+            });
+        });
+    }
+}
+/**
+ * 
+ * @param {String} char 
+ * @returns 
+ */
+const isLetter = (char) => {
+    return char.toLowerCase() >= "a" && char.toLowerCase() <= "z" && char.length == 1;
+}
+/**
+ * @param {String} char 
+ * @returns 
+ */
+const isBackspace = (char) => {
+    return char.toLowerCase() == "backspace";
+}
+/**
+ * @param {String} char 
+ * @returns 
+ */
+const isArrowLeft = (char) => {
+    return char.toLowerCase() == "arrowleft";
+}
+/**
+ * @param {String} char 
+ * @returns 
+ */
+const isArrowRight = (char) => {
+    return char.toLowerCase() == "arrowright";
+}
+/**
+ * @param {String} char 
+ * @returns 
+ */
+const isDelete = (char) => {
+    return char.toLowerCase() == "delete";
+}
+
 /**
  * @param {number} rowIndex 
  * @returns {HTMLInputElement[]}
