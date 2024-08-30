4821c44801e4b8d15c633397ce0120da3a0e45c5
dnlKaizer <danilokaizer@gmail.com>
2024-08-30 12:02:24 -0300

    Colocando tudo em uma função só (dá pra minimizar tudo só minimizando a função geral)

--- script.js
+++ script.js
@@ -1,4 +1,83 @@
-function addLettersEvents() {
+function configLetters() {
+    /**
+     * @param {KeyboardEvent} event
+     * @param {number} i
+     * @param {number} j
+     */
+    const goThroughLetters = (event, i, j) => {
+        const str = event.key;
+        // Verifica os casos abaixo e, se forem válidos, .focus() na próxima letra
+        if ((isArrowRight(str) || isDelete(str)) && (j + 1) < 5) {
+            getLetter(i,j + 1).focus();
+        }
+        // Verifica os casos abaixo e, se forem válidos, .focus() na letra anterior
+        if ((isBackspace(str) || isArrowLeft(str)) && (j - 1) >= 0) {
+            getLetter(i, j - 1).focus();
+        }
+    }
+    /**
+     * @param {number} i 
+     * @param {number} j 
+     */
+    const goNext = (i,j) => {
+        if ((j + 1) < 5) {
+            getLetter(i,j + 1).focus();
+        }
+    }
+    /**
+     * @param {KeyboardEvent} event 
+     */
+    const validateKey = (event) => {
+        if (!isValidKey(event.key)) {
+            event.preventDefault();
+        } 
+    }
+
+    /**
+     * 
+     * @param {String} char 
+     * @returns {boolean}
+     */
+    const isLetter = (char) => {
+        return ((char.toLowerCase() >= "a" && char.toLowerCase() <= "z") || char.toLowerCase() == "ç") && char.length == 1;
+    }
+    /**
+     * @param {String} char 
+     * @returns {boolean}
+     */
+    const isBackspace = (char) => {
+        return char.toLowerCase() == "backspace";
+    }
+    /**
+     * @param {String} char 
+     * @returns {boolean}
+     */
+    const isArrowLeft = (char) => {
+        return char.toLowerCase() == "arrowleft";
+    }
+    /**
+     * @param {String} char 
+     * @returns {boolean}
+     */
+    const isArrowRight = (char) => {
+        return char.toLowerCase() == "arrowright";
+    }
+    /**
+     * @param {String} char 
+     * @returns {boolean}
+     */
+    const isDelete = (char) => {
+        return char.toLowerCase() == "delete";
+    }
+    /**
+     * @param {String} char 
+     * @returns {boolean}
+     */
+    const isValidKey = (char) => {
+        return isLetter(char) || isBackspace(char) || isArrowLeft(char)
+        || isArrowRight(char) || isDelete(char) || char.toLowerCase() == "tab";
+    }
+
     for (let i = 0; i < 6; i++) {
         getWord(i).forEach((element, j) => {
             // Evento disparado quando uma tecla do teclado é solta
@@ -17,81 +96,6 @@ function addLettersEvents() {
             })
         });
     }
-}
-
-/**
- * @param {KeyboardEvent} event
- * @param {number} i
- * @param {number} j
- */
-const goThroughLetters = (event, i, j) => {
-    const str = event.key;
-    // Verifica os casos abaixo e, se forem válidos, .focus() na próxima letra
-    if ((isArrowRight(str) || isDelete(str)) && (j + 1) < 5) {
-        getLetter(i,j + 1).focus();
-    }
-    // Verifica os casos abaixo e, se forem válidos, .focus() na letra anterior
-    if ((isBackspace(str) || isArrowLeft(str)) && (j - 1) >= 0) {
-        getLetter(i, j - 1).focus();
-    }
-}
-const goNext = (i,j) => {
-    if ((j + 1) < 5) {
-        getLetter(i,j + 1).focus();
-    }
-}
-/**
- * @param {KeyboardEvent} event 
- */
-const validateKey = (event) => {
-    if (!isValidKey(event.key)) {
-        event.preventDefault();
-    } 
-}
-
-/**
- * 
- * @param {String} char 
- * @returns {boolean}
- */
-const isLetter = (char) => {
-    return ((char.toLowerCase() >= "a" && char.toLowerCase() <= "z") || char.toLowerCase() == "ç") && char.length == 1;
-}
-/**
- * @param {String} char 
- * @returns {boolean}
- */
-const isBackspace = (char) => {
-    return char.toLowerCase() == "backspace";
-}
-/**
- * @param {String} char 
- * @returns {boolean}
- */
-const isArrowLeft = (char) => {
-    return char.toLowerCase() == "arrowleft";
-}
-/**
- * @param {String} char 
- * @returns {boolean}
- */
-const isArrowRight = (char) => {
-    return char.toLowerCase() == "arrowright";
-}
-/**
- * @param {String} char 
- * @returns {boolean}
- */
-const isDelete = (char) => {
-    return char.toLowerCase() == "delete";
-}
-/**
- * @param {String} char 
- * @returns {boolean}
- */
-const isValidKey = (char) => {
-    return isLetter(char) || isBackspace(char) || isArrowLeft(char)
-    || isArrowRight(char) || isDelete(char) || char.toLowerCase() == "tab";
 }
 
 /**
