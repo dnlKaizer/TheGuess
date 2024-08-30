a8d089c885d0197d65b1ceaf5f2f9f0e03109779
dnlKaizer <danilokaizer@gmail.com>
2024-08-30 10:57:43 -0300

    Métodos de validação de caracteres nos inputs de letters

--- script.js
+++ script.js
@@ -1,59 +1,86 @@
-const rows = 6;
-const cols = 5;
-
-let currentRow = 0;
-let currentCol = 0;
-
 function addLettersEvents() {
     for (let i = 0; i < 6; i++) {
         getWord(i).forEach((element, j) => {
+            // Evento disparado quando uma tecla do teclado é solta
             element.addEventListener("keyup", (event) => {
-                const str = event.key;
-                if ((isLetter(str) || isArrowRight(str) || isDelete(str)) && j + 1 < 6) {
-                    getLetter(i,j + 1).focus();
-                }
-                if ((isBackspace(str) || isArrowLeft(str)) && j - 1 >= 0) {
-                    getLetter(i, j - 1).focus();
-                }
+                goThroughLetters(event, i, j);
             });
+            // Evento disparado quando uma tecla do teclado é pressionada
+            element.addEventListener("keydown", (event) => {
+                validateKey(event);
+            })
         });
     }
 }
+
+/**
+ * @param {KeyboardEvent} event
+ * @param {number} i
+ * @param {number} j
+ */
+const goThroughLetters = (event, i, j) => {
+    const str = event.key;
+    // Verifica os casos abaixo e, se forem válidos, .focus() na próxima letra
+    if ((isLetter(str) || isArrowRight(str) || isDelete(str)) && (j + 1) < 5) {
+        getLetter(i,j + 1).focus();
+    }
+    // Verifica os casos abaixo e, se forem válidos, .focus() na letra anterior
+    if ((isBackspace(str) || isArrowLeft(str)) && (j - 1) >= 0) {
+        getLetter(i, j - 1).focus();
+    }
+}
+/**
+ * @param {KeyboardEvent} event 
+ */
+const validateKey = (event) => {
+    if (!isValidKey(event.key)) {
+        event.preventDefault();
+    } 
+}
+
 /**
  * 
  * @param {String} char 
- * @returns 
+ * @returns {boolean}
  */
 const isLetter = (char) => {
-    return char.toLowerCase() >= "a" && char.toLowerCase() <= "z" && char.length == 1;
+    return ((char.toLowerCase() >= "a" && char.toLowerCase() <= "z") || char.toLowerCase() == "ç") && char.length == 1;
 }
 /**
  * @param {String} char 
- * @returns 
+ * @returns {boolean}
  */
 const isBackspace = (char) => {
     return char.toLowerCase() == "backspace";
 }
 /**
  * @param {String} char 
- * @returns 
+ * @returns {boolean}
  */
 const isArrowLeft = (char) => {
     return char.toLowerCase() == "arrowleft";
 }
 /**
  * @param {String} char 
- * @returns 
+ * @returns {boolean}
  */
 const isArrowRight = (char) => {
     return char.toLowerCase() == "arrowright";
 }
 /**
  * @param {String} char 
- * @returns 
+ * @returns {boolean}
  */
 const isDelete = (char) => {
     return char.toLowerCase() == "delete";
+}
+/**
+ * @param {String} char 
+ * @returns {boolean}
+ */
+const isValidKey = (char) => {
+    return isLetter(char) || isBackspace(char) || isArrowLeft(char)
+    || isArrowRight(char) || isDelete(char) || char.toLowerCase() == "tab";
 }
 
 /**
